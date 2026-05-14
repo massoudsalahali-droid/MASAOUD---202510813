5fed7a57fa3bf4f00b408a4775cb6f172c7a6673
massoudsalahali-droid <massoudsalahali@gmail.com>
2026-05-15 00:22:34 +0200

    Add files via upload

---
+++ Recursive-algorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1)       // Base case
+    return 1;
+    return n * factorial(n - 1); // Recursive call
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "! = " << factorial(n) << endl;
+    return 0;
+}
+    
